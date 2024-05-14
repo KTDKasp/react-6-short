2bbc4cf8bd1894491778f43ef2f9eff2992d9094
KTDKasp <churroskasp@gmail.com>
2024-05-14 20:01:39 +0300

    r6s-modal-window

---
+++ r6s-modal-window/src/App.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.scss';
+
+interface ModalProps {
+	isModalOpen: boolean;
+	setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children?: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isModalOpen, setIsModalOpen, children }) => {
+	return (
+		<div className={`overlay animated ${isModalOpen ? 'show' : ''}`}>
+			<div className="modal">
+				<svg
+					height="200"
+					viewBox="0 0 200 200"
+					width="200"
+					onClick={() => setIsModalOpen(false)}
+				>
+					<title />
+					<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+				</svg>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export const App: React.FC = () => {
+	const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
+
+	return (
+		<div className="App">
+			<button className="open-modal-btn" onClick={() => setIsModalOpen(true)}>
+				✨ Открыть окно
+			</button>
+
+			{/* without animation */}
+			{/* {isModalOpen ? (
+				<div className="overlay">
+					<div className="modal">
+						<svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setIsModalOpen(false)}>
+							<title />
+							<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+						</svg>
+						<img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+					</div>
+				</div>
+			) : null} */}
+
+      {/* with animation */}
+			<Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <h3>Ты открыл окно</h3>
+      </Modal>
+		</div>
+	);
+};
